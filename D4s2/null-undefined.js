abe8bc3737ab5d76ad3c1697862f4f319505315e
Fernando Betanco <fernando.a.betanco@gmail.com>
2020-10-19 21:27:09 -0600

    Problema pitagoras bueno

--- D4s2/null-undefined.js
+++ D4s2/null-undefined.js
@@ -2,8 +2,10 @@ let numberOne = 15;
 
 // a^2 + b^2 = c^2
 function pitagoras(a, b) {
-   return Math.hypot(2, 2);
-    
+    var a= a**2;
+    var b= b**2;
+    var c = a +b;;
+    console.log(c);
 }
+pitagoras(2,2);
 
-console.log(pitagoras);
